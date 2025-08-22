535fd65af97d5772119c0c8b40ce307a81db8f7b
EmiGale <156153460+EmiGale@users.noreply.github.com>
2025-08-22 03:09:06 +0000

    Power should only accept numbers

--- operations.py
+++ operations.py
@@ -29,7 +29,7 @@ if __name__ == "__main__":
 
     print("start test")
 
-    print("Divide no deberia aceptar dividir entre 0")
-    print(divide(100, 0))
+    print("Power solo deberia de aceptar numeros")
+    print(power(100, "asd"))
 
     print("end test")
